10e9a45de3b1812a3f07eaa5666b277d20c528c4
Calvin RIce <ramen.calvin@yahoo.com>
2023-04-24 14:47:08 -0500

    2 Lists

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tempArray:  [String] = ["Medcine 1", "Medicine 2"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {  //Presctiption List
+                List {
+                    ForEach(tempArray, id: \.self) { item in
+                        Text(item)
+                    }
+                    Text("Add New +")
+                }
+            }
+            
+            VStack {  //OTC List
+                List {
+                    ForEach(tempArray, id: \.self) { item in
+                        Text(item)
+                    }
+                    Text("Add New +")
+                }
+            }
         }
     }
 }
