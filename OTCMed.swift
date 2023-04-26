03d65abf80e5e963f230fb1bfa1005daa4ef43b5
Calvin RIce <ramen.calvin@yahoo.com>
2023-04-26 14:19:40 -0500

    added classes for prescriptions and etc meds

---
+++ OTCMed.swift
@@ -0,0 +1,7 @@
+struct OTCMed {
+    var medName: String //Ibuprofen
+    var brandName: String //Advil
+    var doseAmount: Int //mg
+    var doseFrequency: Int //hours
+    var timerRunning: Bool //tracks if the dose timer is running
+}
---
+++ Prescription.swift
@@ -0,0 +1,7 @@
+struct Prescription {
+    var medName: String //Escitalopram
+    var brandName: String //Lexapro
+    var doseAmount: Int //mg
+    var doseFrequency: Int //hours
+    var hasTaken: Bool //tracks if you took it today
+}
