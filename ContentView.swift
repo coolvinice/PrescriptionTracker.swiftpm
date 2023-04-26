9d4810775bd1d3b8f8d6435bbe397720621e2388
Calvin RIce <ramen.calvin@yahoo.com>
2023-04-26 14:06:24 -0500

    Border Rectangles

--- ContentView.swift
+++ ContentView.swift
@@ -3,24 +3,39 @@ import SwiftUI
 struct ContentView: View {
     @State var tempArray:  [String] = ["Medcine 1", "Medicine 2"]
     var body: some View {
-        VStack {
-            VStack {  //Presctiption List
-                List {
-                    ForEach(tempArray, id: \.self) { item in
-                        Text(item)
+        ZStack {
+            VStack {
+                VStack {  //Presctiption List
+                    List {
+                        ForEach(tempArray, id: \.self) { item in
+                            Text(item)
+                        }
+                        Text("Add New +")
                     }
-                    Text("Add New +")
+                }
+                
+                VStack {  //OTC List
+                    List {
+                        ForEach(tempArray, id: \.self) { item in
+                            Text(item)
+                        }
+                        Text("Add New +")
+                    }
                 }
             }
             
-            VStack {  //OTC List
-                List {
-                    ForEach(tempArray, id: \.self) { item in
-                        Text(item)
-                    }
-                    Text("Add New +")
-                }
+            VStack {
+                Rectangle()
+                    .frame(width: .infinity, height: 70)
+                    .foregroundColor(.indigo)
+
+                Spacer()
+                Rectangle()
+                    .frame(width: .infinity, height: 100)
+                    .foregroundColor(.indigo)
             }
+            .ignoresSafeArea()
+            .frame(width: .infinity, height: .infinity)
         }
     }
 }
