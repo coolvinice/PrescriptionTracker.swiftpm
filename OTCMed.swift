ff97fb7b84315704a6b5bef024471b389280d577
Calvin RIce <ramen.calvin@yahoo.com>
2023-05-02 14:25:54 -0500

    doing json stuff

--- OTCMed.swift
+++ OTCMed.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-struct OTCMed: Identifiable {
+struct OTCMed: Identifiable, Codable {
     var id: Int
     var medName: String //Ibuprofen
     var brandName: String //Advil
@@ -8,3 +8,26 @@ struct OTCMed: Identifiable {
     var doseFrequency: Int //hours
     var timerRunning: Bool //tracks if the dose timer is running
 }
+
+struct MyView: View {
+    var body: some View {
+        Button {
+            let variable = 0
+            var encodedList: [Data] = []
+            if let encoded = try? JSONEncoder().encode(variable) {
+                encodedList.append(encoded)
+            }
+
+            if let data = UserDefaults.standard.object(forKey: "OTCMeds") as? Data {
+                if let loadedMed = try? JSONDecoder().decode(OTCMed.self, from: data) {
+                    medName = loadedMed.medName
+                    
+                }
+            }
+        } label: {
+            
+        }
+
+    }
+}
+
