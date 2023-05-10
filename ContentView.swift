9f058fd09e747f5490d6c7a46c4d215843dbb0e0
Calvin RIce <ramen.calvin@yahoo.com>
2023-05-10 14:57:31 -0500

    Making OTC Timer

--- ContentView.swift
+++ ContentView.swift
@@ -13,12 +13,12 @@ struct ContentView: View {
                     List {
                         Section("Prescription") {
                             ForEach($prescriptions) { item in
-                                let _name: String = item.medName.wrappedValue
-                                let _brand: String = item.brandName.wrappedValue
+                                let name: String = item.medName.wrappedValue
+                                let brand: String = item.brandName.wrappedValue
                                 NavigationLink {
                                     EditPrescription(rx: item, rxArray: $prescriptions, otcArray: $otcMeds)
                                 } label: {
-                                    Text("\(_brand) (\(_name))")
+                                    Text("\(brand) (\(name))")
                                 }
                             }
                             NavigationLink {
@@ -30,12 +30,14 @@ struct ContentView: View {
                         
                         Section("Over-the-Counter") {
                             ForEach($otcMeds) { item in
-                                let _name: String = item.medName.wrappedValue
-                                let _brand: String = item.brandName.wrappedValue
+                                let name: String = item.medName.wrappedValue
+                                let brand: String = item.brandName.wrappedValue
+                                let amount: Int = item.doseAmount.wrappedValue
+                                let frequency: Int = item.doseFrequency.wrappedValue
                                 NavigationLink {
                                     EditOTCMed(med: item, rxArray: $prescriptions, otcArray: $otcMeds)
                                 } label: {
-                                    Text("\(_brand) (\(_name))")
+                                    OTCListItem(name: name, brand: brand, amount: amount, frequency: frequency, timeLeft: 75)
                                 }
                             }
                             NavigationLink {
@@ -45,13 +47,12 @@ struct ContentView: View {
                             }
                         }
                     }
-                    
                     .listStyle(.sidebar)
                 }
                 
                 VStack {
                     Rectangle()
-                        .frame(width: .infinity, height: 50)
+                        .frame(width: .infinity, height: 60)
                         .foregroundColor(.indigo)
                     Spacer()
                     Rectangle()
@@ -63,5 +64,34 @@ struct ContentView: View {
             }
         }
         .navigationViewStyle(.stack)
+    }
+}
+
+struct OTCListItem: View {
+    let name: String
+    let brand: String
+    let amount: Int
+    let frequency: Int
+    @State var timeLeft: Int
+    @State var timeString: String = ""
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("\(brand) (\(name))")
+                .font(.title2)
+            Text(verbatim: "\(amount) mg Every \(frequency == 1 ? "Hour" : "\(frequency) Hours") - \(timeString) Until Next Dose")
+        }
+        .onChange(of: timeLeft) { newValue in
+            let minutes = newValue % 60
+            let hours = newValue / 60
+            timeString = "\(hours):\(minutes)"
+        }
+        .onAppear() {
+            let minutes = timeLeft % 60
+            let hours = timeLeft / 60
+            timeString = "\(String(format: "%02d", hours)):\(String(format: "%02d", minutes))"
+        }
+        .onChange(of: Date.now) { newValue in
+//            timeLeft = Date.now.advanced(by: TimeInterval(frequency)).formatted() as Int
+        }
     }
 }
