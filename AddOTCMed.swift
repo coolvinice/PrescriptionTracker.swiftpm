1ab1c49a4a41c69beb7b5e6f91b288db2ec9ec1a
Dylan Domeracki <ddomeracki4103@Stu.d214.org>
2023-05-18 14:41:31 -0500

    pulling

--- AddOTCMed.swift
+++ AddOTCMed.swift
@@ -16,18 +16,43 @@ struct AddOTCMed: View {
                 List {
                     DetailTextField(title: "Brand Name", prompt: "Brand Name", boundString: $brandName)
                         .disableAutocorrection(true)
+                        .font(
+                            .title
+                            .weight(.semibold)
+
+                        )
                     DetailTextField(title: "Medicine Name", prompt: "Medicine Name", boundString: $medicineName)
                         .disableAutocorrection(true)
+                        .font(
+                            .title
+                            .weight(.semibold)
+
+                        )
                     DetailIntField(title: "Dose Amount (mg)", prompt: "Dose Amount", boundVar: $doseAmount)
                         .disableAutocorrection(true)
+                        .font(
+                            .title
+                            .weight(.semibold)
+
+                        )
                     DetailIntField(title: "Dose Frequency (Hours)", prompt: "Dose Frequency", boundVar: $doseFrequency)
                         .disableAutocorrection(true)
+                        .font(
+                            .title
+                            .weight(.semibold)
+
+                        )
                     Button {
                         dismiss()
                         otcMeds.append(OTCMed(id: otcMeds.count, medName: medicineName, brandName: brandName, doseAmount: doseAmount, doseFrequency: doseFrequency, timerRunning: false, lastDose: Date.now))
                         SaveLoad().saveArrays(rx: prescriptions, otc: otcMeds)
                     } label: {
                         Text("Save")
+                            .font(
+                                .title
+                                .weight(.semibold)
+
+                            )
                     }
                 }
             }
