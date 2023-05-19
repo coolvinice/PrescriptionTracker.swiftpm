f3437a7a1f0a1b3a8cc63c376e62d05799946e0d
Calvin RIce <ramen.calvin@yahoo.com>
2023-05-19 09:54:20 -0500

    making display version

--- TimerView.swift
+++ TimerView.swift
@@ -4,22 +4,30 @@ struct TimerView: View {
     @State var nowDate: Date = Date()
     @State var endDate: Date
     @State var referenceDate: Date
+    let setDate: Date
+    var timer: Timer {
+    Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
+    self.nowDate = Date()
+    }
+    }
     
     var body: some View {
-        Text(countDownString(from: referenceDate, until: endDate) + " until next dose")
+        Text(TimerFunction(from: referenceDate) + " until next dose")
             .font(
                 .body
                 .weight(.light)
-
             )
     }
     
-    func countDownString(from date: Date, until nowDate: Date) -> String {
-        let components = Calendar(identifier: .gregorian).dateComponents([.hour, .minute, .second], from: nowDate, to: date)
-        return String(format: "%02d:%02d:%02d",
-                      components.hour ?? 00,
-                      components.minute ?? 00,
-                      components.second ?? 00)
+    func TimerFunction(from date: Date) -> String {
+    let calendar = Calendar(identifier: .gregorian)
+    let timeValue = calendar
+    .dateComponents([.day, .hour, .minute, .second], from: nowDate, to: setDate)
+    return String(format: "%02d days left - %02d:%02d:%02d",
+    timeValue.day!,
+    timeValue.hour!,
+    timeValue.minute!,
+    timeValue.second!)
     }
 }
         
